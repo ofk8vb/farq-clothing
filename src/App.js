1ca299a1ee9203ffc2877fae649bccfdc5f1aee9
ofk8vb <latios_gs@hotmail.com>
2020-08-20 11:55:26 +0300

    checkout page 1 completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx'
+import CheckoutPage from './pages/checkout/checkout.component'
 import {Route, Switch,Redirect} from 'react-router-dom'
 import Header from './components/header/header.component.jsx'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx'
@@ -46,6 +47,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route path='/checkout' component={CheckoutPage} />
           <Route exact path='/signin' render={()=>this.props.currentUser?(<Redirect to='/'/>):(<SignInAndSignUpPage/>)}/>
           </Switch>
       </div>
---
+++ src/pages/checkout/checkout.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './checkout.styles.scss';
+
+const CheckoutPage = () => <div className='title'>CHECKOUT PAGE</div>
+
+export default CheckoutPage;
