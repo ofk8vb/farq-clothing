f26127297d656357e932407447230adbd2ba2f7d
ofk8vb <latios_gs@hotmail.com>
2020-08-24 14:30:32 +0300

    stripe added

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -3,6 +3,8 @@ import './checkout-item.styles.scss'
 import {connect} from 'react-redux'
 import {clearItemFromCart,addItem,removeItem} from '../../redux/cart/cart.actions'
 
+
+
 const CheckoutItem = ({cartItem,clearItem,addItem,removeItem}) => {
     const {name,imageUrl,price,quantity} = cartItem;
 
