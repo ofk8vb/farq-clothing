1f852fcacbe59e5e85a7ae993cceac5456a964eb
ofk8vb <latios_gs@hotmail.com>
2020-08-27 11:40:58 +0300

    redux thunk oncesi

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -23,8 +23,8 @@ class ShopPage extends React.Component{
     const {updateCollections} = this.props;
     const collectionRef = firestore.collection('collections');
 
-    collectionRef.onSnapshot(async snapshot =>{
-      const collectionsMap=convertCollectionsSnapshotToMap(snapshot);
+    collectionRef.get().then(snapshot =>{
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
       this.setState({ loading: false });
     })
