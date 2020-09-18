6bdc99a4c7172f66c2bf5fc35f1f1ebe5e7b7504
ofk8vb <latios_gs@hotmail.com>
2020-09-18 11:10:22 +0300

    bonuslardayim

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React,{Profiler} from 'react';
 import Directory from '../../components/directory/directory.component'
 import {HomePageContainer} from './homepage.styles'
 const HomePage =() =>(
   
       <HomePageContainer>
+        <Profiler id="Directory" onRender={(id,phase,actualDuration)=>{
+          console.log({
+            id,
+            phase,
+            actualDuration
+          })
+        }}>
         <Directory />
+        </Profiler>
       </HomePageContainer>
   
 )
