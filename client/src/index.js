ba23748fb66393c25ca2121f4558e00e52429c53
ofk8vb <latios_gs@hotmail.com>
2020-09-18 17:09:03 +0300

    pwa modified

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import {BrowserRouter} from 'react-router-dom'
 import {Provider} from 'react-redux';
 import {store,persistor} from './redux/store'
 import {PersistGate} from 'redux-persist/integration/react'
-
+import * as serviceWorker from './serviceWorker'
 
 import './index.css';
 import App from './App';
@@ -23,3 +23,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+serviceWorker.register();
